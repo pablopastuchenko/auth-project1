37bf02b4ab18f2d6abb185215d58de7bf7d9e37b
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-14 09:53:23 -0300

    first commit

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" 
+  ? "http://localhost:5000/api/auth" 
+  : "https://auth-project1-ip57.vercel.app/api/auth";
+
 
 axios.defaults.withCredentials = true;
 
