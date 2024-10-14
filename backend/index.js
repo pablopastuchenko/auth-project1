420933aa04fd968db4742133076c2866c51caf90
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-14 10:00:29 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser";
 import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
-
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -14,22 +13,29 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "https://auth-project1-ip57-8xpcdta2h-pastuchenko73s-projects.vercel.app", credentials: true }));
+// Configurando CORS para permitir o frontend hospedado no Vercel
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://auth-project1-ip57-8xpcdta2h-pastuchenko73s-projects.vercel.app"
+];
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
-app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+app.use(express.json()); // permite analisar o corpo das requisições
+app.use(cookieParser()); // permite analisar cookies
 
 app.use("/api/auth", authRoutes);
 
+// Servir o frontend em produção
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
